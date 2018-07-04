ef2594f4925ded145a2fbabf35572720fa49bece
imran-khaan <38178449+imran-khaan@users.noreply.github.com>
2018-07-04 19:58:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/imran/androidphpmysql/Constants.java
@@ -0,0 +1,14 @@
+package com.example.imran.androidphpmysql;
+
+/**
+ * Created by Imran on 9/25/2017.
+ */
+
+public class Constants {
+    private static  final String ROOT_URL = "http://172.16.4.20/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
